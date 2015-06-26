ccc810d50d47065c99ea65a395929aba07c653f5
jk409 <jk409@qq.com>
2015-06-26 19:22:12 +0800

    Update typing.py

--- mod/typing.py
+++ mod/typing.py
@@ -2,14 +2,16 @@ __author__ = 'Administrator'
 import sys,time,socket
 #E-mail:jk409@qq.com
 #example: test.py   ip   80
+ip   = sys.argv[1]
+port = sys.argv[2]
+timeout = 0.5
 while 1:
     #time.sleep(0.8)
     try:
         time.sleep(1)
         sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        ip = sys.argv[1]
-        port = int(sys.argv[2])
-        sc.settimeout(0.5)
+        port = int(port)
+        sc.settimeout(timeout)
         sc.connect((ip,port))
         print (ip,':',port,':OK')
     except socket.timeout:
